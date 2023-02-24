9fdeea16573bd409af3fa4449e45071844f05092
Michael Green <greenmr95@hotmail.com>
2023-02-23 20:30:06 -0800

    complete count odd numbers

---
+++ programming-skills/1.count-odd-numbers.js
@@ -0,0 +1,26 @@
+// Count Odd Numbers in an Interval Range
+
+// Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive). 
+
+// Example 1:
+// Input: low = 3, high = 7
+// Output: 3
+// Explanation: The odd numbers between 3 and 7 are [3,5,7].
+
+// Example 2:
+// Input: low = 8, high = 10
+// Output: 1
+// Explanation: The odd numbers between 8 and 10 are[9].
+
+const countOdds = function(low, high) {
+    let count = 0
+    for (i = low; i <= high; i++) {
+        if (i % 2 != 0) {
+            count ++
+        }
+    }
+    return count
+};
+
+console.log(countOdds(3, 7))  // 3
+console.log(countOdds(8, 10)) // 1
