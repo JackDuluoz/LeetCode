852221203a658c3525930057e9514aa83cd3c03a
Michael Green <greenmr95@hotmail.com>
2023-02-24 09:02:56 -0800

    complete reverse string

---
+++ algorithms/8.reverse-string.js
@@ -0,0 +1,21 @@
+// Reverse String
+
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+// Example 1:
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o", "l", "l", "e", "h"]
+
+// Example 2:
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+
+const reverseString = function(s) {
+  s.reverse()
+  return s
+};
+
+console.log(reverseString(["h","e","l","l","o"]))       // ["o", "l", "l", "e", "h"]
+console.log(reverseString(["H","a","n","n","a","h"]))   // ["h","a","n","n","a","H"]
