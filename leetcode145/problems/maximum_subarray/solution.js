109c106f5bd17456326db565069a29dd000da15c
Michael Green <greenmr95@hotmail.com>
2023-02-25 08:43:47 +0000

    Sync LeetCode submission - Maximum Subarray (javascript)

--- leetcode145/problems/maximum_subarray/solution.js
+++ leetcode145/problems/maximum_subarray/solution.js
@@ -4,22 +4,16 @@
  */
 
 const maxSubArray = function(nums) {
-        let currSum = 0;
-        let maxSum = Math.min(...nums);
-
-        for (i = 0; i < nums.length; i++) {
-            currSum += nums[i];
-
-            if (currSum > maxSum) {
-            maxSum = currSum;
-            }
-
-            if (currSum < 0) {
-            currSum = 0;
-            }
+    let currSum = 0;
+    let maxSum = Math.min(...nums);
+    for (i = 0; i < nums.length; i++) {
+        currSum += nums[i];
+        if (currSum > maxSum) {
+        maxSum = currSum;
         }
-
-        return maxSum;
-
-
+        if (currSum < 0) {
+        currSum = 0;
+        }
+    }
+    return maxSum;
 };
