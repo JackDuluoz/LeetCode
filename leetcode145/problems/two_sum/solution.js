a75d3ac8a89d423e6d626f8bb43f985745aca69f
Michael Green <greenmr95@hotmail.com>
2023-04-04 21:55:40 +0000

    Sync LeetCode submission - Two Sum (javascript)

--- leetcode145/problems/two_sum/solution.js
+++ leetcode145/problems/two_sum/solution.js
@@ -5,14 +5,13 @@
  */
 
 const twoSum = function(nums, target) {
-  let arr = []
+    
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
-        arr.push(i)
-        arr.push(j)
+        return [i, j]
       }
     }
   }
-    return arr
+
 };
